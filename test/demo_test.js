1f4d2d43d12793a5412db1cd27658f9d47fe5e62
Omar Salem <omar@omarsalempro.com>
2019-05-20 14:53:33 -0600

    Add mocha for testing purposes.

---
+++ test/demo_test.js
@@ -0,0 +1,12 @@
+const MarioChar = require('../models/mariochar');
+const assert = require('assert');
+
+// Describe tests
+describe('Some Demo Test' , () => {
+
+    // Create test
+    it('Add two numbers together', () => {
+        assert(2+3 === 5);
+    })
+
+});
