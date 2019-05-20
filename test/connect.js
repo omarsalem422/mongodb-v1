f4381cd79ef61885363e36680e89e11432016d2e
Omar Salem <omar@omarsalempro.com>
2019-05-20 17:03:26 -0600

    add code to drop collection

--- test/connect.js
+++ test/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-before((done) => {
+before((done) => {   //Mocha hook
     //Connect to mongodb
     mongoose.connect('mongodb://localhost:27017/testaroo', { useNewUrlParser: true });
 
@@ -12,5 +12,14 @@ before((done) => {
         .once('error', (error) => {
             console.log('Connection error: ', error);
         });
+});
+
+
+// Drop the character collection before each test
+beforeEach( (done) => {     //Mocha hook
+    mongoose.connection.collections.mariochars.drop( () => {
+        done();
+    })
+    
 });
 
